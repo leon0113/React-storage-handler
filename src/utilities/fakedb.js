58d15a35deb047fd0435632049e9b67f45a1d433
leon0113 <tahjib.leon399@gmail.com>
2022-11-05 23:10:18 +0600

    'localstorageadd'

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,18 +1,30 @@
 // manage cart data in local storage
 const addToDb = id => {
-    let shoppingCart = {};
 
-    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
-    console.log(typeof shoppingCart);
-
-    const quantity = localStorage.getItem(id); //id is the key. getItem() gives you the value
-    if (quantity) {
-        const newQuantity = parseInt(quantity) + 1;
-        // localStorage.setItem(id, newQuantity);
+    let shoppingCart;
+    //! get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart');
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
     }
     else {
+        shoppingCart = {};
+    }
+
+    //! add quantity
+    // const quantity = localStorage.getItem(id); //id is the key. getItem() gives you the value
+    const quantity = shoppingCart[id]; // it gives you the value of id
+    if (quantity) {
+        const newQuantity = quantity + 1;
+        // localStorage.setItem(id, newQuantity);
+        shoppingCart[id] = newQuantity;
+    }
+    else {
+        shoppingCart[id] = 1;
         // localStorage.setItem(id, 1);
     }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+
 
 }
 
